0b545201be9d0a3f427a4052924f36d744e55e05
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-26 18:55:12 -0500

    Agregar archivos de proyecto.

---
+++ BLOGUdlaProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BLOGUdlaProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
